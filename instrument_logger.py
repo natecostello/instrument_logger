dc6f6b8be513b2946d1df529bb6baaa1f056c981
Nate Costello <natecostello@gmail.com>
2021-07-03 01:35:27 +0000

    added some unit testing and bug fixes

--- instrument_logger.py
+++ instrument_logger.py
@@ -5,131 +5,165 @@ from abc import ABC, abstractmethod
 from threading import Thread
 
 class Instrument(ABC):
+    """An abstract class that provides an interface for objects to use InstrumentLogger"""
 
     @property
     @abstractmethod
     def name(self):
         pass
 
+    @property
     @abstractmethod
-    def getMeasurement(self, name: str) -> str:
-        """Returns the most recent measurement for 'instrument_name.parameter_name.parameter_unit"""
-        pass
-
-    @abstractmethod
-    def getAllMeasurements(self) -> 'dict':
+    def allmeasurements(self) -> 'dict':
         """Returns a dict of most recent measurements keyed by 'instrument_name.parameter_name.parameter_unit'"""
         pass
-
+    
+    @property
     @abstractmethod
-    def getParameters(self) -> 'list[str]':
+    def parameters(self) -> 'list[str]':
         """Returns a list of parameters measured with each entry of the form 'instrument_name.parameter_name.parameter_unit'.  The elements of this list are always in the same order."""
         pass
 
+    @abstractmethod
+    def getmeasurement(self, name: str) -> str:
+        """Returns the most recent measurement for 'instrument_name.parameter_name.parameter_unit"""
+        pass
 
 class InstrumentLogger:
+    """A class that is assigned Instruments and timestamps and logs thier parameters to csv files."""
 
     def __init__(self):
-        self._frequency = 1
+        self._frequency = 1.0
         self._header = ''
         self._lastlogdict = {}
         self._lastlogentry = ''
         self._lastlogparams = []
         self._lastlogtime = ''
         self._filenameprefix = ''
-        self._fileHandler = None
+        self._filehandler = None
         self._logger = None
         self._instruments = []
-        self._loggingThread = None
-        self._keepLogging = False
-        self._uniqueInstanceName = str(datetime.now())
+        self._loggingthread = None
+        self._keeplogging = False
+        self._unique_instance_name = str(datetime.now()) #insures this instance's logger is unique
+        self._filename = ''
 
-        
-
-    def __initializeLogger(self, time: str):
+    def __initializelogger(self, time: str):
 
         # set header
         header = 'time,'
         for inst in self._instruments:
-            for parameter in inst.getParameters():
+            for parameter in inst.parameters:
                 header += parameter + ','
         self._header = header[:-1]
 
         # set logger
-        logger = logging.getLogger(self._uniqueInstanceName) 
+        logger = logging.getLogger(self._unique_instance_name) 
         logger.setLevel(logging.DEBUG)
         self._logger = logger
 
         # set fileHandler
         filename = self._filenameprefix + '_' + time.replace(" ", "_").replace(":", "_") + ".csv"
         fh = logging.FileHandler(filename, delay=True)
+        self._filename = filename
         fh.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(message)s')
         fh.setFormatter(formatter)
-        self._fileHandler = fh
+        self._filehandler = fh
         self._logger.addHandler(fh)
         self._logger.info(self._header)
 
-    def __terminateLogger(self):
+    def __terminatelogger(self):
         if self._logger:
-            self._logger.removeHandler(self._fileHandler)
+            self._logger.removeHandler(self._filehandler)
             self._logger = None
-            self._fileHandler = None
+            self._filehandler = None
             self._header = ''
+            self._filename = ''
 
-
-    def __newLogFile(self):
-        filename = self._filenameprefix + '_' + str(datetime.now).replace(" ", "_").replace(":", "_") + ".csv"
-        fh = logging.FileHandler(filename, delay=True)
-        fh.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(message)s')
-        fh.setFormatter(formatter)
-        self._fileHandler = fh
-        self._logger.addHandler(fh)
-        self._logger.info(self._header)
-
-    def __createHeader(self):
-        header = 'time,'
-        for inst in self._instruments:
-            for parameter in inst.getParameters():
-                header += parameter + ','
-        self._header = header
-        
-
-    @property
-    def frequency(self):
-        """Get the logging frequency"""
-        return self._frequency
-
-    @frequency.setter
-    def frequency(self, seconds: float):
-        """Set the logging frequency"""
-        self._frequency = seconds
-
-    def __logContinuous(self):
+    def __logcontinuous(self):
         # TODO: think about how to keep from slowly drifting
-        while self._keepLogging:
+        while self._keeplogging:
             if (datetime.now() >= self._lastlogtime + timedelta(seconds=self._frequency)):
                 self.log()
             else:
                 sleep(0.1)
 
+    @property
+    def frequency(self) -> float:
+        """Get the logging frequency, default is 1.0"""
+        return self._frequency
+
+    @frequency.setter
+    def frequency(self, seconds: float):
+        """Set the logging frequency"""
+        self._frequency = float(seconds)
+
+    @property
+    def lastlogdict(self) -> dict:
+        """A dict of all last logged values keyed by 'instrument_name.parameter_name.parameter_unit' or empty dict"""
+        return dict(self._lastlogdict)
+
+    @property
+    def lastlogparams(self) -> list:
+        """A list of all last logged params of format 'instrument_name.parameter_name.parameter_unit' or empty list"""
+        return list(self._lastlogparams)
+
+    @property
+    def lastlogentry(self) -> str:
+        """A string of csv values from the last log entry or empty string"""
+        return self._lastlogentry
+
+    @property
+    def headerstring(self):
+        """Get a string of csv headers of format 'time,instrument_name.parameter_name.parameter_unit,...' or empty string"""
+        return self._header
+
+    @property
+    def filenameprefix(self) -> str:
+        """Get the filename prefix, default is empty string"""
+        return self._filenameprefix
+    
+    @filenameprefix.setter
+    def filenameprefix(self, prefix: str):
+        """Filenames are by default named from the time of creation.  
+        A prefix adds descriptive text to the filename.  Will interrupt logging and start a new log file."""
+        self._filenameprefix = prefix
+        self.newlog()
+    
+    @property
+    def filename(self) -> str:
+        """The filename currently in use for logging"""
+        return self._filename
+
+    def addinstrument(self, instrument: Instrument):
+        """Add an instrument to be logged.  Will stop continuous logging.  
+        Any future logging will be written to a new file."""
+        self.newlog()
+        self._instruments.append(instrument)
+
+    def newlog(self):
+        """Start a new log file for next logged event.  Will stop continous logging."""
+        self.stop()
+        sleep(0.2)
+        self.__terminatelogger()
+
     def start(self):
         """Start continuous logging"""
-        self._keepLogging = True
-        self._loggingThread = Thread(target=self.__logContinuous)
-        self._loggingThread.start()
+        self._keeplogging = True
+        self._loggingthread = Thread(target=self.__logcontinuous)
+        self._loggingthread.start()
 
     def stop(self):
         """Stop continuous logging."""
-        self._keepLogging = False
+        self._keeplogging = False
 
     def log(self):
         """Create a single log entry with the current instrument measurements"""
 
         # Check to make sure there are instruments if not, stop
         if len(self._instruments) == 0:
-            self._keepLogging = False
+            self._keeplogging = False
             return
         
         self._lastlogtime = datetime.now()
@@ -137,7 +171,7 @@ class InstrumentLogger:
 
         # Check to make sure there is a logger initialized if not initialize
         if not self._logger:
-            self.__initializeLogger(time)
+            self.__initializelogger(time)
 
         logentry = ''
         logdict = {}
@@ -148,8 +182,8 @@ class InstrumentLogger:
         logdict['time']=time
 
         for inst in self._instruments:
-            measurements = inst.getAllMeasurements()
-            parameters = inst.getParameters()
+            measurements = inst.allmeasurements
+            parameters = inst.parameters
             for param in parameters:
                 logentry += measurements[param] + ','
                 logparams.append(param)
@@ -161,45 +195,3 @@ class InstrumentLogger:
         self._lastlogdict = logdict
 
         self._logger.info(logentry)
-
-
-    def getLastLogDict(self):
-        """Get a dict of all last logged values keyed by 'instrument_name.parameter_name.parameter_unit'"""
-        return dict(self._lastlogdict)
-
-    def getLastLogParams(self):
-        """Get a list of all last logged params of format 'instrument_name.parameter_name.parameter_unit'"""
-        return list(self._lastlogparams)
-
-    def getLastLogEntry(self):
-        """Get a string of csv values from the last log entry"""
-        return self._lastlogentry
-
-    def getHeaderString(self):
-        """Get a string of csv headers of format 'instrument_name.parameter_name.parameter_unit'"""
-        return self._header
-
-    def addInstrument(self, instrument: Instrument):
-        """Add an instrument to be logged.  Will stop continuous logging.  Any future logging will be written to a new file."""
-        self.newLog()
-        self._instruments.append(instrument)
-
-    def newLog(self):
-        """Start a new log file for next logged event.  Will stop continous logging."""
-        self.stop()
-        sleep(0.2)
-        self.__terminateLogger()
-
-
-    def setFilenamePrefix(self, prefix: str):
-        """Filenames are by default named from the time of creation.  
-        This adds a descriptive prefix to the file name.  Will start a new log file."""
-        self._filenameprefix = prefix
-        self.newLog()
-
-
-
-
-
-
-
--- tests/test_instrument_logger.py
+++ tests/test_instrument_logger.py
@@ -5,17 +5,30 @@ import random
 import math
 import os
 
+import unittest
+
+
+param1_name = 'param1'
+param1_unit = 'unit1'
+param1_val = 1.0
+param2_name = 'param2'
+param2_unit = 'unit2'
+param2_val = 2.0
+test_inst_name = 'test_inst'
+all_params = {
+    test_inst_name + '.' + param1_name + '.' + param1_unit : str(param1_val),
+    test_inst_name + '.' + param2_name + '.' + param2_unit : str(param2_val)}
 
 class TestInstrument(Instrument):
 
-    def __init__(self, name) -> None:
+    def __init__(self, name):
         self._name = name
-        self.param1_name = 'param1'
-        self.param1_unit = '[unit1]'
-        self.param1_val = str(math.floor(random.random()*100))
-        self.param2_name = 'param2'
-        self.param2_unit = '[unit2]'
-        self.param2_val = str(math.floor(random.random()*100))
+        self.param1_name = param1_name
+        self.param1_unit = param1_unit
+        self.param1_val = param1_val
+        self.param2_name = param2_name
+        self.param2_unit = param2_unit
+        self.param2_val = param2_val
         self.all_params = { 
             self.name + '.' + self.param1_name + '.' + self.param1_unit : str(self.param1_val),
             self.name + '.' + self.param2_name + '.' + self.param2_unit : str(self.param2_val)
@@ -25,20 +38,80 @@ class TestInstrument(Instrument):
     def name(self):
         return self._name
     
-    def getMeasurement(self, name):
+    def getmeasurement(self, name):
         return self.all_params[name]
 
-    def getAllMeasurements(self):
-        self.param1_val = str(math.floor(random.random()*100))
-        self.param2_val = str(math.floor(random.random()*100))
-        self.all_params = { 
-            self.name + '.' + self.param1_name + '.' + self.param1_unit : str(self.param1_val),
-            self.name + '.' + self.param2_name + '.' + self.param2_unit : str(self.param2_val)
-        }
+    @property
+    def allmeasurements(self):
+        # self.param1_val = str(math.floor(random.random()*100))
+        # self.param2_val = str(math.floor(random.random()*100))
+        # self.all_params = { 
+        #     self.name + '.' + self.param1_name + '.' + self.param1_unit : str(self.param1_val),
+        #     self.name + '.' + self.param2_name + '.' + self.param2_unit : str(self.param2_val)
+        # }
         return self.all_params
 
-    def getParameters(self):
+    @property
+    def parameters(self):
         return list(self.all_params.keys())
+
+
+class InstrumentTestCase(unittest.TestCase):
+    def setUp(self):
+        self.instrument = TestInstrument(test_inst_name)
+    
+    def test_instrument_name(self):
+        self.assertEqual(self.instrument.name, test_inst_name)
+
+    def test_instrument_getmeasurement(self):
+        self.assertEqual(self.instrument.getmeasurement(test_inst_name + '.' + param1_name + '.' + param1_unit), str(param1_val))
+    
+    def test_instrument_allmeasurements(self):
+        self.assertEqual(self.instrument.all_params, all_params)
+
+class InstrumentLoggerConstructorTestCase(unittest.TestCase):
+    def setUp(self):
+        self.instrument = TestInstrument(test_inst_name)
+        self.inst_logger = InstrumentLogger()
+    
+    def test_frequency_prop(self):
+        self.assertEqual(self.inst_logger.frequency, 1.0)
+
+    def test_frequency_setter(self):
+        old_freq = self.inst_logger.frequency
+        new_freq = 0.5
+        self.inst_logger.frequency = new_freq
+        self.assertEqual(self.inst_logger.frequency, new_freq)
+        self.inst_logger.frequency = old_freq
+    
+    def test_lastlogdict(self):
+        self.assertEqual(self.inst_logger.lastlogdict,{})
+    
+    def test_lastlogparams(self):
+        self.assertEqual(self.inst_logger.lastlogparams, [])
+    
+    def test_lastlogentry(self):
+        self.assertEqual(self.inst_logger.lastlogentry, '')
+    
+    def test_headerstring(self):
+        self.assertEqual(self.inst_logger.headerstring, '')
+
+    def test_filenameprefix(self):
+        self.assertEqual(self.inst_logger.filenameprefix, '')
+    
+    def test_filenameprefix_setter(self):
+        old_prefix = self.inst_logger.filenameprefix
+        new_prefix = 'alajflsdfj'
+        self.inst_logger.filenameprefix = new_prefix
+        self.assertEqual(self.inst_logger.filenameprefix, new_prefix)
+        self.inst_logger.filenameprefix = old_prefix
+    
+    def test_filename(self):
+        self.assertEqual(self.inst_logger.filename, '')
+    
+
+    
+
 
 print('Testing...')
 os.system('rm *.csv')
@@ -47,65 +120,65 @@ print('Testing Instrument instantiation')
 test_inst = TestInstrument('test_inst_1')
 
 print('Testing getParameters')
-params = test_inst.getParameters()
+params = test_inst.parameters
 print(params)
 
 print('Testing getMeasurement')
-print(test_inst.getMeasurement(params[0]))
+print(test_inst.getmeasurement(params[0]))
 
 print('Testing getAllMeasurement')
-print(test_inst.getAllMeasurements())
+print(test_inst.allmeasurements)
 
 print('Testing InstrumentLogger instantiation')
 test_logger = InstrumentLogger()
 
 print('Testing addInstrument')
-test_logger.addInstrument(test_inst)
+test_logger.addinstrument(test_inst)
 
 print('Test adding file prefix')
-test_logger.setFilenamePrefix('name')
+test_logger.filenameprefix = 'name'
 
 print('Test single log')
 test_logger.log()
 
-# print('Test another single log')
-# test_logger.log()
+print('Test another single log')
+test_logger.log()
 
-# print('Test continous for 5 seconds')
-# test_logger.start()
-# sleep(5)
-# test_logger.stop()
+print('Test continous for 5 seconds')
+test_logger.start()
+sleep(5)
+test_logger.stop()
 
-# print('Test adding new file prefix while logging')
-# test_logger.start()
-# sleep(1)
-# test_logger.setFilenamePrefix('name2')
+print('Test adding new file prefix while logging')
+test_logger.start()
+sleep(1)
+test_logger.filenameprefix = 'name2'
 
-# print('Test continous for 5 seconds')
-# test_logger.start()
-# sleep(5)
-# test_logger.stop()
+print('Test continous for 5 seconds')
+test_logger.start()
+sleep(5)
+test_logger.stop()
 
-# print('Test adding a second instrument')
-# test_inst2 = TestInstrument('test_inst_2')
-# test_logger.addInstrument(test_inst2)
+print('Test adding a second instrument')
+test_inst2 = TestInstrument('test_inst_2')
+test_logger.addinstrument(test_inst2)
 
-# print('Test continous for 5 seconds')
-# test_logger.start()
-# sleep(5)
-# test_logger.stop()
+print('Test continous for 5 seconds')
+test_logger.start()
+sleep(5)
+test_logger.stop()
 
-# print('Test higher frequency')
-# test_logger.frequency = .5
+print('Test higher frequency')
+test_logger.frequency = .5
 
-# print('Test continous for 5 seconds')
-# test_logger.start()
-# sleep(5)
-# test_logger.stop()
+print('Test continous for 5 seconds')
+test_logger.start()
+sleep(5)
+test_logger.stop()
 
 print('Test logger with no instruments')
 
 print('Testing InstrumentLogger instantiation')
 test_logger = InstrumentLogger()
-test_logger.setFilenamePrefix('name')
+test_logger.filenameprefix = 'name' 
 test_logger.log()
